405027e8a2f06843a806d9258b349fb48f54ad5a
Denis <denisbutko2003@gmail.com>
2023-09-16 21:00:08 +0300

    done

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 // const dist = "./dist/";
-const dist = `C:/MAMP/htdocs/test`;
+const dist = `C:/MAMP/htdocs/vasvikna`;
 
 gulp.task("copy-html", () => {
 		return gulp.src("./src/index.html")
